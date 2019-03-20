690dbb0737f5434883ec35e224b0e9e57eef6b82
ChatNoir76 <julien.riviere47@orange.fr>
2019-03-20 11:29:24 +0100

    création du projet via visual basic express 2010

---
+++ VBTools/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de 
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Passez en revue les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("VBTools")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("FAREVA")> 
+<Assembly: AssemblyProduct("VBTools")> 
+<Assembly: AssemblyCopyright("Copyright © FAREVA 2019")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("14b051ac-b96d-40eb-aad5-9ec69c3b55e9")> 
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire 
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
