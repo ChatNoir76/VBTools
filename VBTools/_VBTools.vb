d454ab97fdd41c717678e0a95684cf0606ea6bab
ChatNoir76 <julien.riviere47@orange.fr>
2019-03-28 12:28:35 +0100

    version 1.1.0.0

--- VBTools/_VBTools.vb
+++ VBTools/_VBTools.vb
@@ -2,9 +2,9 @@
 Imports VBTools.GestionDataGridView
 
 'mettre le fichier texte à jour pour le changement de version
-<Assembly: Reflection.AssemblyFileVersion("1.0.1.0")> 
-<Assembly: Reflection.AssemblyVersion("1.0.1.0")> 
-<Assembly: Reflection.AssemblyInformationalVersion("Build 2")> 
+<Assembly: Reflection.AssemblyFileVersion("1.1.0.0")> 
+<Assembly: Reflection.AssemblyVersion("1.1.0.0")> 
+<Assembly: Reflection.AssemblyInformationalVersion("Build 3")> 
 
 Module _VBTools
     'application console pour test de la dll
